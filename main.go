d579256d1994eb520ac9390fabdf8b5999634d80
Nishanth Shetty <nishanth.shetty@go-jek.com>
2021-03-04 15:27:20 +0530

    use builder to get the viper instance

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	c "nishanth.io/vipersss/config"
 
@@ -10,31 +11,33 @@ import (
 
 func main() {
 	// Set the file name of the configurations file
-	viper.SetConfigName("config")
-	viper.SetConfigType("yml")
+	vipe := viper.NewWithOptions(viper.KeyDelimiter("_"))
+	vipe.SetConfigName("config")
+	vipe.SetConfigType("yml")
 
 	// Set the path to look for the configurations file
-	viper.AddConfigPath(".")
-	viper.KeyDelimiter("_")
+	vipe.AddConfigPath(".")
+
+	vipe.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 
 	// Enable VIPER to read Environment Variables
-	viper.AutomaticEnv()
+	vipe.AutomaticEnv()
 
 	var configuration c.Configurations
 
-	if err := viper.ReadInConfig(); err != nil {
+	if err := vipe.ReadInConfig(); err != nil {
 		fmt.Printf("Error reading config file, %s", err)
 	}
 
 	// Set undefined variables
-	viper.SetDefault("database.dbname", "test_db")
+	vipe.SetDefault("database.dbname", "test_db")
 
-	err := viper.Unmarshal(&configuration)
+	err := vipe.Unmarshal(&configuration)
 	if err != nil {
 		fmt.Printf("Unable to decode into struct, %v", err)
 	}
 
-	fmt.Println(viper.GetViper().AllKeys())
+	fmt.Println(vipe.AllKeys())
 	// Reading variables using the model
 	fmt.Println("Reading variables using the model..")
 	fmt.Println("Database is\t", configuration.Database.DBName)
@@ -45,10 +48,10 @@ func main() {
 
 	// Reading variables without using the model
 	fmt.Println("\nReading variables without using the model..")
-	fmt.Println("Database is\t", viper.GetString("database.dbname"))
-	fmt.Println("server Port is\t\t", viper.GetInt("server.port"))
-	fmt.Println("server Port is\t\t", viper.GetInt("SERVER_PORT"))
-	fmt.Println("EXAMPLE_PATH is\t", viper.GetString("EXAMPLE_PATH"))
-	fmt.Println("EXAMPLE_VAR is\t", viper.GetString("EXAMPLE_VAR"))
+	fmt.Println("Database is\t", vipe.GetString("database.dbname"))
+	fmt.Println("server Port is\t\t", vipe.GetInt("server.port"))
+	fmt.Println("server Port is\t\t", vipe.GetInt("SERVER_PORT"))
+	fmt.Println("EXAMPLE_PATH is\t", vipe.GetString("EXAMPLE_PATH"))
+	fmt.Println("EXAMPLE_VAR is\t", vipe.GetString("EXAMPLE_VAR"))
 
 }
